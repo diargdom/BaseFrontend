288c9f47ea392342e9aed7131a169d479069ee0d
Diego Argumedo <alejokun10@outlook.com>
2026-04-27 15:20:04 -0500

    Feat: Se crea el proyecto de react

---
+++ React/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gray-700 text-yellow-400 text-center">
+        Proyecto base de react
+      </div>
+    </>
+  );
+}
+
+export default App;
